03b82fa98cee135973515402a21671cb3837d834
Josh Hart <joshua.hart@protonmail.com>
2020-04-17 12:10:36 +0100

    added play/pause button. Created changeText function and applied it

--- script.js
+++ script.js
@@ -6,24 +6,42 @@ console.log(images)
 let counter = 0;
 const scrollWidth = images[0].clientWidth;
 
-//transition slide
+//transition slide functions
 const transitionSlideForward = () => { 
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    carouselSlide.style.transition = "transform 0.6s ease-in-out";
     counter++;
     carouselSlide.style.transform = "translateX(" + -scrollWidth * counter + "px)";
 
 }
 
 const transitionSlideBackward= () => { 
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    carouselSlide.style.transition = "transform 0.6s ease-in-out";
     counter--;
     carouselSlide.style.transform = "translateX(" + -scrollWidth * counter + "px)";
 
 }
 
+// automatic play function
 
 const autoLoop = () => {
     setInterval(transitionSlideForward, 3000);
   }
 
-  autoLoop();
+
+autoLoop();
+
+//PlayPause button text change
+
+const button = document.getElementById('playpause');
+
+const changeText = () => {
+    if (button.innerText === "Play") {
+        button.innerText = "Pause";
+    } else {
+        button.innerText = "Play";
+    }
+}
+
+button.addEventListener("click", changeText);
+
+
