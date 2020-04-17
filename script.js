abad69dc7149210edf465aeff9fbb696901865ed
Josh Hart <joshua.hart@protonmail.com>
2020-04-17 10:22:32 +0100

    created automatic forward slide function. Carousel is skipping second photo for some reason

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const carouselSlide = document.querySelector(".my-slides");
 const images = document.querySelectorAll(".my-slides img");
-console.log(images);
+console.log(images)
 
 // counter
 let counter = 1;
 const scrollWidth = images[0].clientWidth;
+
+//transition slide
+const transitionSlideForward = () => { 
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+    carouselSlide.style.transform = "translateX(" + -scrollWidth * counter + "px)";
+}
+
+function myFunction() {
+    setInterval(function(){ transitionSlideForward(); }, 3000);
+  }
+
+myFunction();
